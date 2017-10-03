5642ac951b81cbbc66002ed38297a058b4115134
Heri Tri <hery.trie@gmail.com>
2017-10-03 22:01:02 +0700

    set followlocation

--- config/app.php
+++ config/app.php
@@ -18,7 +18,8 @@ class app{
 		curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0");
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+		//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION,true);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 		curl_setopt($ch, CURLOPT_REFERER, 'http://www.frozenshop.com');
 		curl_setopt($ch, CURLOPT_URL, $url);
